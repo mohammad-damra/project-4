509f87296f9db5251034d8786ea0a027bd80b0ba
mohammad-damra <mohammad.s.damra@gmail.com>
2020-11-18 17:55:34 +0200

    Update Function in React

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 const mainRouter = require('./routes/main-route');
-// const cors = require('cors');
+const cors = require('cors');
 const db = require('./db');
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(mainRouter);
 
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,65 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import NewItem from './components/NewItem';
+import ArticleItem from './components/ArticleItem';
 import './App.css';
 
-// Class component
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div>
-        <h1>HELLO WORLD</h1>
-      </div>
-    );
-  }
+import axios from 'axios';
+
+export default function App() {
+	// just for study
+	const [ count, setCount ] = useState(0);
+	const add1 = () => {
+		setCount(count + 1);
+	};
+
+	const [ articles, setArticles ] = useState([]);
+
+	const getAllArticles = () => {
+		axios
+			.get(`http://localhost:5000/articles`)
+			.then((response) => {
+				// console.log('RESPONSE: ', response);
+				console.log('DATA: ', response.data);
+				setArticles(response.data);
+			})
+			.catch((err) => {
+				console.log('ERR: ', err);
+			});
+	};
+
+	const renderArticles = articles.map((articleObj) => {
+		return <ArticleItem article={articleObj} getArticles={getAllArticles} />;
+	});
+
+	return (
+		<div className="app">
+			<h3>APP 1+1 {1 + 1}</h3>
+			{/* just for study this two lines */}
+
+			<h1>Count is: {count} </h1>
+			<button onClick={add1}>Add 1 to the counter</button>
+
+			<button onClick={getAllArticles}>GET ARTICLES</button>
+
+			<NewItem getArticles={getAllArticles} />
+
+			{renderArticles}
+		</div>
+	);
 }
 
 /* 
-// functional component
-const App = () => {
-  return (
-    <div>
-      <h1>HELLO WORLD</h1>
-    </div>
-  );
-};
-export default App;
+state
+lifecyle methods
+
+*/
+/* 
+axios
+    .get(`http://localhost:5000/articles`)
+    .then((response) => {
+      console.log('result: ', response);
+    })
+    .catch((err) => {
+      console.log('ERR: ', err);
+    });
 */
